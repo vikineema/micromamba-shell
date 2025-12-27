375cd68947dcdc757f206523ebf5c1c6e56cc659
Victoria Neema <victoria@kartoza.com>
2025-12-27 13:29:19 +0300

    Update nix to 25.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Micromamba shell environment using Nix and FHS";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # NixOS official package source, using the nixos-25.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs, ... }:
