6fe23b2af3a99bbaa48b60284534578b3060cba2
Victoria Neema <victoria@kartoza.com>
2026-04-10 23:47:06 +0300

    Test simplified nix package

--- flake.nix
+++ flake.nix
@@ -8,38 +8,53 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      micromamba-fhs-name = "micromamba-shell";
+      micromamba-fhs-version = "v1.0.0";
+      micromamba-fhs = pkgs.buildFHSEnv {
+        pname = micromamba-fhs-name;
+        version = micromamba-fhs-version;
+        targetPkgs =
+          pkgs: with pkgs; [
+            micromamba
+            bash-completion
+          ];
+        profile = ''
+          # Silence 'complete' command errors if the builtin isn't ready
+          type complete &>/dev/null || complete() { :; }
+
+          # Copied from micromamba shell init -s bash -r ~/micromamba
+          # >>> mamba initialize >>>
+          # !! Contents within this block are managed by 'micromamba shell init' !!
+          export MAMBA_EXE='${pkgs.micromamba}/bin/micromamba';
+          # export MAMBA_ROOT_PREFIX='${builtins.getEnv "HOME"}/micromamba';
+          export MAMBA_ROOT_PREFIX="$HOME/micromamba";
+          __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+          if [ $? -eq 0 ]; then
+              eval "$__mamba_setup"
+          else
+              alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+          fi
+          unset __mamba_setup
+          # <<< mamba initialize <<<
+
+        '';
+        # Force bash to execute the /etc/profile every time.
+        runScript = "bash --login --rcfile /etc/profile";
+      };
     in
-    {
-      devShells.x86_64-linux.default =
-        (pkgs.buildFHSEnv {
-          pname = "micromamba-fhs-env";
-          version = "v0.1.0";
-          targetPkgs =
-            pkgs: with pkgs; [
-              micromamba
-              bash-completion
-            ];
-          profile = ''
-            # Silence 'complete' command errors if the builtin isn't ready
-            type complete &>/dev/null || complete() { :; }
+    { 
+      # Installable package
+      packages.${system}.default = micromamba-fhs;
+      # Used for nix-develop
+      devShells.${system}.default = pkgs.mkShell {
+        name = micromamba-fhs-name + "-" + micromamba-fhs-version;
+        buildInputs = [
+          micromamba-fhs
+        ];
+        shellHook = ''
+          exec ${micromamba-fhs-name}
+        '';
+      };
 
-            # Copied from micromamba shell init -s bash -r ~/micromamba
-            # >>> mamba initialize >>>
-            # !! Contents within this block are managed by 'micromamba shell init' !!
-            export MAMBA_EXE='${pkgs.micromamba}/bin/micromamba';
-            export MAMBA_ROOT_PREFIX='${builtins.getEnv "HOME"}/micromamba';
-            __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-            if [ $? -eq 0 ]; then
-                eval "$__mamba_setup"
-            else
-                alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
-            fi
-            unset __mamba_setup
-            # <<< mamba initialize <<<
-
-          '';
-          # Force bash to execute the /etc/profile every time.
-          runScript = "bash --login --rcfile /etc/profile";
-        }).env;
     };
 }
