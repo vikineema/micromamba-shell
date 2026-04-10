57fdfdc8935f42a09130925e3b608541d21b4cdf
Victoria Neema <victoria@kartoza.com>
2026-04-10 23:13:24 +0300

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -1,48 +1,45 @@
 {
-  description = "Micromamba shell environment using Nix and FHS";
+  description = "micromamba wrapped in FHS environment";
 
-  inputs = {
-    # NixOS official package source, using the nixos-25.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux"];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.x86_64-linux.default =
+        (pkgs.buildFHSEnv {
+          pname = "micromamba-fhs-env";
+          version = "v0.1.0";
+          targetPkgs =
+            pkgs: with pkgs; [
+              micromamba
+              bash-completion
+            ];
+          profile = ''
+            # Silence 'complete' command errors if the builtin isn't ready
+            type complete &>/dev/null || complete() { :; }
 
-      pkgsFor = forAllSystems (system: import nixpkgs {
-        localSystem = system;
-        config.allowUnfree = false;
-      });
+            # Copied from micromamba shell init -s bash -r ~/micromamba
+            # >>> mamba initialize >>>
+            # !! Contents within this block are managed by 'micromamba shell init' !!
+            export MAMBA_EXE='${pkgs.micromamba}/bin/micromamba';
+            export MAMBA_ROOT_PREFIX='${builtins.getEnv "HOME"}/micromamba';
+            __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+            if [ $? -eq 0 ]; then
+                eval "$__mamba_setup"
+            else
+                alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+            fi
+            unset __mamba_setup
+            # <<< mamba initialize <<<
 
-    in  {
-    ######################################################
-    ##
-    ## Package Definitions
-    ##
-    ######################################################
-    packages = forAllSystems (system: 
-      let 
-        pkgs = pkgsFor.${system}; 
-      in rec {
-        micromamba-shell = pkgs.callPackage ./packages/micromamba-shell { };
-        default = micromamba-shell;
-      }
-    );
-    
-    ######################################################
-    ##
-    ## Apps
-    ##
-    ######################################################
-    apps = forAllSystems (system: {
-        micromamba-shell = {
-          type = "app";
-          program = "${self.packages.${system}.micromamba-shell}/bin/micromamba-shell";
-        };
-
-        default = self.apps.${system}.micromamba-shell;
-    });
-  };
+          '';
+          # Force bash to execute the /etc/profile every time.
+          runScript = "bash --login --rcfile /etc/profile";
+        }).env;
+    };
 }
